23c411eee08131e83f43af401dc9036e83aaf0ed
HJKCC <chencheng0816@gmail.com>
2018-10-12 10:56:07 +0800

    add a UML Model of the strategy-pattern

---
+++ src/main/java/com/cc/strategy/service/FlyBehavior.java
@@ -0,0 +1,10 @@
+package com.cc.strategy.service;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @Description FlyBehavior
+ * @date 2018/10/11 17:21
+ */
+public interface FlyBehavior {
+	public void fly();
+}
---
+++ src/main/java/com/cc/strategy/service/QuackBehavior.java
@@ -0,0 +1,10 @@
+package com.cc.strategy.service;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @Description QuackBehavior
+ * @date 2018/10/11 17:21
+ */
+public interface QuackBehavior {
+	public void quack();
+}
