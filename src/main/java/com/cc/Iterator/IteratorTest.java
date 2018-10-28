b9ebe9e998e12bafa27a5f267e0fe4de75af6bed
HJKCC <chencheng0816@gmail.com>
2018-10-28 21:17:28 +0800

    add the iterator-pattern

---
+++ src/main/java/com/cc/Iterator/IteratorTest.java
@@ -0,0 +1,13 @@
+package com.cc.Iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @date 2018/10/28 15:16
+ * @Description IteratorTest
+ */
+public class IteratorTest {
+	List<String> aa = new ArrayList<String>();
+}
