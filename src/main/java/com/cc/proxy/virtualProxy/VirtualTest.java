b3127b2d8ff68fd0ed846aed82892f2539dc3f40
HJKCC <chencheng0816@gmail.com>
2018-11-04 21:50:45 +0800

    modify the virtual proxy

--- src/main/java/com/cc/proxy/virtualProxy/VirtualTest.java
+++ src/main/java/com/cc/proxy/virtualProxy/VirtualTest.java
@@ -6,4 +6,7 @@ package com.cc.proxy.virtualProxy;
  * @Description VirtualTest
  */
 public class VirtualTest {
+	public static void main(String[] args) {
+		System.out.println("VirtualTest");
+	}
 }
