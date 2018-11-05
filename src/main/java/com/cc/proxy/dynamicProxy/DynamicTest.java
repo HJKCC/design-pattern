036f0bee9678e075a285b5959945b403456d1b84
HJKCC <chencheng0816@gmail.com>
2018-11-05 20:13:58 +0800

    add the virtual proxy

--- src/main/java/com/cc/proxy/dynamicProxy/DynamicTest.java
+++ src/main/java/com/cc/proxy/dynamicProxy/DynamicTest.java
@@ -3,7 +3,7 @@ package com.cc.proxy.dynamicProxy;
 /**
  * @author chencheng0816@gmail.com
  * @date 2018/11/5 20:11
- * @Description DynamicTest
+ * @Description DynamicTest 动态代理
  */
 public class DynamicTest {
 }
