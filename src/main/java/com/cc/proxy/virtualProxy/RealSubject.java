21bff3d0ae901bfaa3a602e1e620f47fad7e7902
HJKCC <chencheng0816@gmail.com>
2018-11-05 20:08:46 +0800

    modify the UML diagram of virtual proxy

---
+++ src/main/java/com/cc/proxy/virtualProxy/RealSubject.java
@@ -0,0 +1,9 @@
+package com.cc.proxy.virtualProxy;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @date 2018/11/5 19:29
+ * @Description RealSubject
+ */
+public class RealSubject {
+}
---
+++ src/main/java/com/cc/proxy/virtualProxy/SubjectInterface.java
@@ -0,0 +1,9 @@
+package com.cc.proxy.virtualProxy;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @date 2018/11/5 19:28
+ * @Description SubjectInterface
+ */
+public interface SubjectInterface {
+}
