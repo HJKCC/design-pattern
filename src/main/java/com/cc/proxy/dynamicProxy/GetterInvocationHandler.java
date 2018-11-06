35b863fbcb17bd7062dcc2937d28b42c8cded50f
HJKCC <chencheng0816@gmail.com>
2018-11-06 19:08:09 +0800

    add the dynamic proxy

---
+++ src/main/java/com/cc/proxy/dynamicProxy/GetterInvocationHandler.java
@@ -0,0 +1,9 @@
+package com.cc.proxy.dynamicProxy;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @date 2018/11/6 17:06
+ * @Description GetterInvocationHandler
+ */
+public class GetterInvocationHandler {
+}
---
+++ src/main/java/com/cc/proxy/dynamicProxy/RealSubject.java
@@ -0,0 +1,9 @@
+package com.cc.proxy.dynamicProxy;
+
+/**
+ * @author chencheng0816@gmail.com
+ * @date 2018/11/6 17:02
+ * @Description RealSubject
+ */
+public class RealSubject {
+}
