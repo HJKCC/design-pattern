f7a2439a394988c06d3021c2d0c12ea1cd5a1e47
HJKCC <chencheng0816@gmail.com>
2018-11-07 20:05:19 +0800

    add the MVC pattern

--- src/main/java/com/cc/mvc/MVCTest.java
+++ src/main/java/com/cc/mvc/MVCTest.java
@@ -3,7 +3,10 @@ package com.cc.mvc;
 /**
  * @author chencheng0816@gmail.com
  * @date 2018/11/7 20:02
- * @Description MVCTest
+ * @Description 模型使用观察者模式，控制器是视图的策略，视图使用组合模式实现界面
  */
 public class MVCTest {
+	public static void main(String[] args) {
+		System.out.println("HELLO MVC");
+	}
 }
